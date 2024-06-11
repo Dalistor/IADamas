71dd7ece5df25a86c6737e9fca0cbdeda7aed019
Diego <101148313+Dalistor@users.noreply.github.com>
2024-06-11 17:48:37 -0300

    Adição do gráfico

--- src/app/static/js/table.js
+++ src/app/static/js/table.js
@@ -1,3 +1,6 @@
+let index = 0;
+let lastRow = 0;
+
 new Vue({
     el: '#app',
     data: {
@@ -12,5 +15,42 @@ new Vue({
             [' ', 'P', ' ', 'P', ' ', 'P', ' ', 'P'],
             ['P', ' ', 'P', ' ', 'P', ' ', 'P', ' ']
         ]
+    },
+    methods: {
+        //Criar classes para o tabuleiro
+        makeClass(cell, rowIndex) {
+            let className;
+            let cellPosition;
+            let repeat;
+
+            switch (cell) {
+                case ' ':
+                    className = 'nothing'
+                    break;
+
+                case 'B':
+                    className = 'bot'
+                    break;
+
+                case 'P':
+                    className = 'person'
+                    break;
+
+                default:
+                    className = 'nothing'
+            }
+
+            if (lastRow != rowIndex) {
+                repeat = true;
+                lastRow = rowIndex;
+            } else {
+                repeat = false;
+            }
+
+            index = repeat ? index : index + 1
+            cellPosition = index % 2 === 0 ? 'green' : 'white';
+            
+            return `cell ${className} ${cellPosition}`
+        }
     }
 });
